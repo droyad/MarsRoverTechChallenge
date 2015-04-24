812a6f6ee6215689764c89e34f82b794f8df5042
Robert Wagner <robert@wagner.id.au>
2015-04-24 10:47:55 +1000

    sln and test base

---
+++ MarsRoverTechChallenge/MarsRoverTechChallenge/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace MarsRoverTechChallenge
+{
+    public abstract class TestBase
+    {
+        protected abstract string Run(string input);
+
+        [TestCase]
+        public void BaseCase()
+        {
+            const string input = "5 5\r\n1 2 N\r\nLMLMLMLMM\r\n3 3 E";
+            const string expected = "1 3 N\r\n5 1 E";
+
+            var result = Run(input);
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
